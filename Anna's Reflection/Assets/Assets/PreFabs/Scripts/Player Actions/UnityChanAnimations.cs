aef4ff3b6248e0d490ea37cc8814110695824831
IrmaChan <irma.chan.96@gmail.com>
2018-03-03 13:09:14 +0300

    Animations fixed. Walk animasyonları çalışıyor artık.

--- Anna's Reflection/Assets/Assets/PreFabs/Scripts/Player Actions/UnityChanAnimations.cs
+++ Anna's Reflection/Assets/Assets/PreFabs/Scripts/Player Actions/UnityChanAnimations.cs
@@ -4,27 +4,46 @@ using System.Collections;
 public class UnityChanAnimations : MonoBehaviour {
     public Animator anim;
     public Rigidbody rigid;
-    public float ms;
+    public float msx, msz;
+
+    private bool run;
     // Use this for initialization
     void Start ()
     {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
-        ms = 2f;
+        msx = 1.5f;
+        msz = 2f;
+        run = false;
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(moveHorizontal * ms * Time.deltaTime, 0f, moveVertical * ms * Time.deltaTime);
+        Vector3 move = new Vector3(moveHorizontal * msx * Time.deltaTime, 0f, moveVertical * msz * Time.deltaTime);
+       
+        if(move.z <= 0f)
+        {
+            move.x = 0f;
+        }
+        else if(run)
+        {
+            move.x *= 2f;
+            move.z *= 2f;
+        }
         rigid.MovePosition(transform.position + move);
-
         anim.SetFloat("inputH", moveHorizontal);
         anim.SetFloat("inputV", moveVertical);
-        if (moveHorizontal != 0 || moveVertical != 0)
-            anim.SetBool("isMoving", true);
+        
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
         else
-            anim.SetBool("isMoving", false);
+        {
+            run = false;
+        }
+        anim.SetBool("run", run);
     }
     // Update is called once per frame
     void Update ()
